a86a166e08cd753d08d0e4e512cdeb63b7fc7009
Yukizaka <littletomatons@gmail.com>
2022-05-06 00:44:21 +0900

    added songlist.html
    GASからリスト取得
    テーブル作成

---
+++ main.js
@@ -0,0 +1,23 @@
+var song_list;
+var song_list_num;
+function songAllCallBack(){
+	fetch("https://script.google.com/macros/s/AKfycbwy2CgiAolYHfMzIFTw0E3k2ajFL1RKi_u0mE57ufuGsoDrfkY/exec")
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			// デバッグ用
+			song_list = data;
+			song_list_num = data.length;
+		})
+		.catch(error => {
+			console.log("失敗しました");
+		});
+    setTimeout(()=>{
+		var $myList = $('#songlist');
+		for(var i = 0; i < song_list_num; i++){
+			$myList.append($('<tr><td>'+song_list[i].Artist+'</td><td>'+song_list[i].Song+'</td></tr>'));
+		}
+    },5000);
+}
+songAllCallBack();
