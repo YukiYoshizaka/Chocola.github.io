66c83a3a2b43a58515489f873548053bcab1a030
Yukizaka <littletomatons@gmail.com>
2022-05-08 19:53:44 +0900

    v0.91 upload
    ・ソングリストを歌手でまとめる

--- songlist.js
+++ songlist.js
@@ -13,13 +13,49 @@ request.open('GET', URL, false);
 // レスポンスが返ってきた時の処理を記述 
 request.onload = function () {
 	// レスポンスが返ってきた時の処理
-	song_list = JSON.parse(request.response);
-    console.log(song_list);
+	data = JSON.parse(request.response);
+	song_list = formating_DataArtist(data);
+	console.log(song_list);
 	for(var i = 0; i < song_list.length; i++){
-		$myList.append($('<tr><td class="song_artist"><div class="tb_td"><span class="song_under">'+song_list[i].Song+'</span><br><span class="artist_under">'+song_list[i].Artist+'</span></div></td><td></dt></tr>'));
+		$myList.append($('<tr><td class="song_artist"><div class="tb_td">'));
+		for(var k = 0; k < song_list[i].Song.length; k++){
+			$myList.append($('<span class="song_under">'+song_list[i].Song[k]+'</span><br>'));
+		}
+		$myList.append($('<span class="artist_under">'+song_list[i].Artist+'</span></div></td><td></dt></tr>'));
 		$myList.hide().fadeIn(1500);
 	}
 }
 
 // リクエストをURLに送信
 request.send();
+
+function formating_DataArtist(data){
+	var artist_data = [];
+	var result = [];
+	var temp_song = [];
+	for(var i = 0; i < data.length; i++){
+		artist_data.push(data[i].Artist);
+	}
+
+	// artist_dataの重複削除
+	artist_data = new Set(artist_data);
+	artist_data = Array.from(artist_data);
+	console.log(artist_data);
+
+	for(var i = 0; i < artist_data.length; i++){
+		for(var k = 0; k < data.length; k++){
+			if(artist_data[i] == data[k].Artist){
+				// result[artist_data[k]] = data[i].Song;
+				temp_song.push(data[k].Song);
+			}
+		}
+		result.push(
+			{
+				Artist:artist_data[i],
+				Song:temp_song
+			}
+		);
+		temp_song = [];
+	}
+	return result;
+}
