944db013357ed1b497de0d7ac9b7a3788b661451
Yukizaka <littletomatons@gmail.com>
2022-12-17 00:19:33 +0900

    v0.95 update
    曲リスト周りの修正

--- songlist.js
+++ songlist.js
@@ -1,6 +1,7 @@
 var song_list;
 var song_list_num;
 var $myList = $('#songlist');
+var $linkList = $('#linklist');
 
 var URL = "https://script.google.com/macros/s/AKfycbwy2CgiAolYHfMzIFTw0E3k2ajFL1RKi_u0mE57ufuGsoDrfkY/exec?sheetNo=1";
 
@@ -15,17 +16,22 @@ request.onload = function () {
 	// レスポンスが返ってきた時の処理
 	data = JSON.parse(request.response);
 	song_list = formating_DataArtist(data);
-	console.log(song_list);
+	
+	// 一文字目が変わったらidを追加してジャンプリンクできるようにする
+	var first_song_word = "";
+	var array_song_word = [];
+	var link_data = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
 	for(var i = 0; i < song_list.length; i++){
-		$myList.append($('<tr><td class="song_artist"><span class="artist_under">'+song_list[i].Artist+'</span>'));
-		for(var k = 0; k < song_list[i].Song.length; k++){
-			$myList.append($('<span class="song_under">'+song_list[i].Song[k]+'</span><br>'));
+		
+		for(var k = 0;k < song_list[i].Song.length; k++){
+			// $myList.append($('<tr><td class="song_artist"><span class="artist_under"><a href="https://www.homepage-tukurikata.com/html/a-href.html">'+song_list[i].Artist+' / '+song_list[i].Song[k]+'</a></span><td></td>'));
+			$myList.append($('<li><a href="'+song_list[i].URL+'">'+song_list[i].Artist+' / '+song_list[i].Song[k]+'</a></li>'));
 		}
-		$myList.append($('</td></tr>'));
-		$myList.hide().fadeIn(1500);
+		$myList.hide().fadeIn(500);
+
 	}
 }
-
 // リクエストをURLに送信
 request.send();
 
@@ -33,6 +39,8 @@ function formating_DataArtist(data){
 	var artist_data = [];
 	var result = [];
 	var temp_song = [];
+	var temp_url = [];
+	console.log(data[0]);
 	for(var i = 0; i < data.length; i++){
 		artist_data.push(data[i].Artist);
 	}
@@ -41,22 +49,26 @@ function formating_DataArtist(data){
 	artist_data = new Set(artist_data);
 	artist_data = Array.from(artist_data);
 	artist_data = artist_data.sort();
-	console.log(artist_data);
+	// console.log(artist_data);
 
 	for(var i = 0; i < artist_data.length; i++){
 		for(var k = 0; k < data.length; k++){
 			if(artist_data[i] == data[k].Artist){
 				// result[artist_data[k]] = data[i].Song;
 				temp_song.push(data[k].Song);
+				temp_url.push(data[k].URL);
 			}
 		}
+		
 		result.push(
 			{
 				Artist:artist_data[i],
-				Song:temp_song
+				Song:temp_song,
+				URL:temp_url
 			}
 		);
 		temp_song = [];
+		temp_url = [];
 	}
 	return result;
 }
